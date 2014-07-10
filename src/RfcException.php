2d6eded9c5ff2e338a4f7d6c3a28d60a23a7910a
Martijn Braam <martijn@brixit.nl>
2014-07-10 10:04:50 +0200

    Added exception class for invalid files

---
+++ src/RfcException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace HalClient;
+
+
+class RfcException extends \Exception
+{
+    // Redefine the exception so message isn't optional
+    public function __construct($message, $code = 0, \Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+
+} 
